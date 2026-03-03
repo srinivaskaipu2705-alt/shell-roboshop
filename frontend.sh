49502ef24825c0b68053a76e6a786136cc018ee9
srini <srinivaskaipu2705@gmail.com>
2026-03-03 14:49:55 -0500

    roboshop

---
+++ frontend.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[0;33m'
+B='\033[0;34m'
+N='\033[0m' # No Color
+
+LOGS_FOLDER="/var/log/shell-roboshop" # Define the logs folder path
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # Define the logs file name based on the script name
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define the full path to the logs file
+MONGODB_HOST="mongodb.srini.store" # Define the MongoDB host address
+SCRIPTS_DIR=$PWD # Define the directory where the scripts are located
+mkdir -p $LOGS_FOLDER # Create the logs folder if it doesn't exist
+
+echo "$(date): Starting the script execution..." | tee -a $LOGS_FILE # Log the start of the script execution
+
+if [ $USERID -eq 0 ]; then
+    echo -e "$B I am root user $N"
+    else 
+    echo -e "$R error:: you are not root user, please run this script as root user $N"
+    exit 1
+fi
+
+VALIDATE(){ # function to validate the exit status of the last command
+    if [ $1 -eq 0 ]; then
+        echo -e "$G $2  successful $N" | tee -a $LOGS_FILE # Log the success message to the logs file
+    else 
+        echo -e "$R error:: $2  failed $N" | tee -a $LOGS_FILE # Log the error message to the logs file
+        exit 1
+    fi
+}
+
+#nodejs installation
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS module"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20 module"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJS"
+
+systemctl enable nginx &>>$LOGS_FILE
+VALIDATE $? "Enabling nginx" 
+
+systemctl start nginx &>>$LOGS_FILE
+VALIDATE $? "Starting nginx service"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGS_FILE
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "Downloading frontend application artifact"
+
+cd /usr/share/nginx/html &>>$LOGS_FILE
+VALIDATE $? "Changing to nginx html directory"
+
+unzip /tmp/frontend.zip &>>$LOGS_FILE
+VALIDATE $? "Extracting frontend application artifact"
+
+rm -rf /etc/nginx/nginx.conf &>>$LOGS_FILE
+VALIDATE $? "Removing default nginx configuration file"
+
+cp $SCRIPTS_DIR/config/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
+VALIDATE $? "Copying nginx configuration file"
+
+systemctl restart nginx &>>$LOGS_FILE
+VALIDATE $? "Restarting nginx service"
